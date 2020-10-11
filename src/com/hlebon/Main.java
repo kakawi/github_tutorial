036d5b34bc5f5d21691d2664fe725b4f881d9ca2
demosocket <demosocket@gmail.com>
2020-10-11 10:36:00 +0300

    [ticket-2] bad changes

--- src/com/hlebon/Main.java
+++ src/com/hlebon/Main.java
@@ -3,7 +3,7 @@ package com.hlebon;
 public class Main {
 
     /**
-     * TICKET-2
+     * TICKET-2 Here My bad changes from demosocket
      */
     public static void main(String[] args) {
 	// write your code here
