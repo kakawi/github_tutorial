c859a64a9541ad9ff4c6c35302a812c0c0dcae83
AnzhelikaK <AnzhelikaShch11@gmail.com>
2020-10-11 11:47:51 +0300

    commit F

--- src/com/hlebon/Main.java
+++ src/com/hlebon/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     /**
      * TICKET-1 very important changes
+     * Add comments
+     * second commit
+     * third commit
+     * jkhkjuh     *
+     * commit 7
      */
     public static void main(String[] args) {
 	// write your code here
